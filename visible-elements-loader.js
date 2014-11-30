1d86c7db780141a84d36702ba18b582c6eb2235d
Piotr Bukowski <firesoft1983@gmail.com>
2014-12-01 00:21:54 +0100

    private methods begin with underscore, bug fixes

--- visible-elements-loader.js
+++ visible-elements-loader.js
@@ -9,30 +9,32 @@ function VisibleElementsLoader(params) {
 	this._waitTime = params.waitTime || 800;
 	this._lastCheckTime = 0;
 	this._timeoutId = null;
+
+	this._bindEvents();
 }
 
-VisibleElementsLoader.prototype.bindEvents = function() {
+VisibleElementsLoader.prototype._bindEvents = function() {
 	this.$(window).off('scroll.visible-elements-loader').on('scroll.visible-elements-loader', this.loadVisibleElements.bind(this));
 	this.$(window).off('resize.visible-elements-loader').on('resize.visible-elements-loader', this.loadVisibleElements.bind(this));
 }
 
 
-
 VisibleElementsLoader.prototype.loadVisibleElements = function() {
 	if (this._isInWaitTime()) {
-		this.initTimeoutCheck();
+		this._initTimeoutCheck();
+		return;
 	}
-	
+
 	this._lastCheckTime = Date.now();
 	this._timeoutId = null;
 
-	var element = this.getElementToLoad();
+	var element = this._getElementToLoad();
 	if (!element) {
 		return;
 	}
-	this.setElementAsLoaded(element);
-	this.loadElementData(element);
-	this.initTimeoutCheck();
+	this._setElementAsLoaded(element);
+	this._loadElementData(element);
+	this._initTimeoutCheck();
 }
 
 VisibleElementsLoader.prototype._isInWaitTime = function() {
@@ -40,70 +42,70 @@ VisibleElementsLoader.prototype._isInWaitTime = function() {
 }
 
 
-VisibleElementsLoader.prototype.initTimeoutCheck = function() {
+VisibleElementsLoader.prototype._initTimeoutCheck = function() {
 	if (this._timeoutId) {
 		return;
 	}
 	this._timeoutId = setTimeout(this.loadVisibleElements.bind(this), this._waitTime);
 }
 
-VisibleElementsLoader.prototype.setElementAsLoaded = function(element) {
+VisibleElementsLoader.prototype._setElementAsLoaded = function(element) {
 	element.data('loader-status','done');
 }
 
-VisibleElementsLoader.prototype.loadElementData = function(element) {
+VisibleElementsLoader.prototype._loadElementData = function(element) {
 	var src = element.data('ajax-source');
 	if (!src) {
 		return;
 	}
 	this.$.getJSON(src, function(data) {
-		this.injectData(element, data);
+		this._injectData(element, data);
 	});;
 }
 
-VisibleElementsLoader.prototype.injectData = function(element, data) {
+VisibleElementsLoader.prototype._injectData = function(element, data) {
 	if (!data.html) {
 		return '';
 	}
 	element.html(data.html);
 }
 
-VisibleElementsLoader.prototype.getElementToLoad = function() {
-	var elements = this.getElementsToLoad();
+VisibleElementsLoader.prototype._getElementToLoad = function() {
+	var elements = this._getElementsToLoad();
 	if (!elements.length) {
 		return;
 	}
 	return elements[0];
 }
 
-VisibleElementsLoader.prototype.getElementsToLoad = function() {
-	return this.$(this._selector).filter(this.isElementValidToLoad.bind(this)).toArray().sort(this.compareElements.bind(this));
+VisibleElementsLoader.prototype._getElementsToLoad = function() {
+	return this.$(this._selector).filter(this._isElementValidToLoad.bind(this)).toArray().sort(this._compareElements.bind(this));
 }
 
-VisibleElementsLoader.prototype.isElementValidToLoad = function(index, element) {
-	if (!this.isElementVisible(element)) {
+VisibleElementsLoader.prototype._isElementValidToLoad = function(index, element) {
+	if (!this._isElementVisible(element)) {
 		return false;
 	}
-	return !isELementLoaded(element);
+	return !this._isELementLoaded(element);
 }
 
-VisibleElementsLoader.prototype.isELementLoaded = function(element) {
+VisibleElementsLoader.prototype._isELementLoaded = function(element) {
 	return (this.$(element).data('loader-status') == 'done');
 }
 
-VisibleElementsLoader.prototype.isElementVisible = function(element) {
+VisibleElementsLoader.prototype._isElementVisible = function(element) {
     var docViewTop = this.$(window).scrollTop();
     var docViewBottom = docViewTop + this.$(window).height();
-    var elemTop = this.getElementTop(element);
+    var elemTop = this._getElementTop(element);
 
     return elemTop <= docViewBottom;
 }
 
-VisibleElementsLoader.prototype.getElementTop = function(element) {
+VisibleElementsLoader.prototype._getElementTop = function(element) {
 	return this.$(element).offset().top;	
 }
 
-VisibleElementsLoader.prototype.compareElements = function(element1, element2) {
+VisibleElementsLoader.prototype._compareElements = function(element1, element2) {
 	return this.getElementTop(element1) - this.getElementTop(element2);
 }
 
